5dae5da16fb7dc2b034063145bfa03b00ac70e17
Jordan Hatcher <jhatch12@gmail.com>
2018-07-18 21:46:21 -0400

    Fixed checking branch in body

--- src/listener.py
+++ src/listener.py
@@ -18,19 +18,22 @@ except:
 @app.route(config['route'], methods=['POST'])
 def webhook():
     try:
-        body = request.get_json()
+        body = request.get_data()
+        body_dict = json.loads(body.decode('utf-8'))
     except:
-        return ('', 400)
+        return ('', 500)
 
-    if body['ref'] == 'refs/heads/master':
+    if body_dict['ref'] == 'refs/heads/master':
         payload_signature = request.headers.get('X-Hub-Signature').replace('sha1=', '')
-        calculated_signature = hmac.new(config['secret'].encode(), request.get_data(), sha1).hexdigest()
+        calculated_signature = hmac.new(config['secret'].encode(), body, sha1).hexdigest()
 
         if hmac.compare_digest(payload_signature.encode(), calculated_signature.encode()):
             os.system('\n'.join(config['hook']))
             return ('OK', 200)
+        else:
+            return ('Unauthorized', 401)
+    return ('Unsupported branch', 200)
 
-    return ('', 400)
 
 if __name__ == "__main__":
     app.run(host=config['host'], port=config['port'])
